125704ddb01aa3dbef63ba1ec5e7aa40a2f3c883
HARMESH095 <harmeshgopinathan@gmail.com>
2025-07-11 01:20:36 +0530

    Update: working well updated scripts to extract all data and put it right into the csv

--- backend/config.py
+++ backend/config.py
@@ -9,7 +9,7 @@ VECTOR_DIR = os.path.join(DATA_DIR, "Vector", "symptom_faiss_db")  # fixed path
 # File paths
 TEST_CASES_CSV = os.path.join(RAW_DIR, "test_symptom_cases.csv")
 LABELS_JSON = os.path.join(RAW_DIR, "labels.json")
-MAYO_CSV = os.path.join(BASE_DIR, "mayo_diseases.csv")  # since this is in notebook/
+MAYO_CSV = os.path.join(BASE_DIR, "data","mayo_diseases.csv")  # since this is in notebook/
 FAISS_INDEX = os.path.join(VECTOR_DIR, "index.faiss")
 FAISS_META = os.path.join(VECTOR_DIR, "index.pkl")
     
