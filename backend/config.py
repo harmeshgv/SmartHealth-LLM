9aa40d2029d1cc9810943f59011dfc18b9c66437
HARMESH095 <harmeshgopinathan@gmail.com>
2025-07-20 21:48:01 +0530

    update : updated the path of the test csv path

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ RAW_DIR = DATA_DIR  # because your labels.json and test_symptom_cases.csv are di
 VECTOR_DIR = os.path.join(DATA_DIR, "Vector", "symptom_faiss_db")  # fixed path
 
 # File paths
-TEST_CASES_CSV = os.path.join(RAW_DIR, "test_symptom_cases.csv")
+TEST_CASES_CSV = os.path.join(BASE_DIR, "data","mayo_diseases.csv")
 LABELS_JSON = os.path.join(RAW_DIR, "labels.json")
 MAYO_CSV = os.path.join(BASE_DIR, "data","mayo_diseases.csv")  # since this is in notebook/
 FAISS_INDEX = os.path.join(VECTOR_DIR, "index.faiss")
