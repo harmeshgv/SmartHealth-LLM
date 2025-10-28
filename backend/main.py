133a6166db7c34a3d381054a1fd1b11d738d2384
harmeshgv <harmeshgopinathan@gmail.com>
2025-10-28 19:42:09 +0530

    complete update push

--- backend/main.py
+++ backend/main.py
@@ -39,6 +39,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://harmeshgv.github.io/SmartHealth-LLM/",
     ],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],
